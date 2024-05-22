a5e7627aee76ecac9b7cda07d49e0cd1986bdd74
tamo-tmry <mollianium@gmail.com>
2024-05-23 00:19:58 +0900

    feat: update settings

--- wezterm.lua
+++ wezterm.lua
@@ -2,19 +2,30 @@ local wezterm = require 'wezterm';
 local act = wezterm.action
 
 return {
-  -- font = wezterm.font('Cica'),
   font = wezterm.font {
     family = 'Cica',
-    weight = 'Regular',
+    weight = 'Bold',
     style = 'Italic'
   },
   use_ime = true,
-  font_size = 24.0,
+  font_size = 18.0,
   -- color_scheme = 'OneHalfDark',
   color_scheme = 'Gruvbox Dark',
   hide_tab_bar_if_only_one_tab = true,
   adjust_window_size_when_changing_font_size = false,
-  window_background_opacity = 0.8,
+  background = {
+    {
+      source = {
+        File = '/Image/Path',
+      },
+      hsb =  { brightness = 0.04 },
+      opacity = 1.0,
+      height = 'Contain',
+      width = '100%',
+      repeat_x = 'NoRepeat',
+      horizontal_align = 'Center',
+    }
+  },
   keys = {
     {
       key = 'v',
